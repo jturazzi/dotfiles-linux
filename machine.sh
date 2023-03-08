d05bed70fca6c4de1dc568981a1bc172fe7e3065
Jérémy Turazzi <git@jtrz.fr>
2023-03-08 21:09:39 +0000

    Commit Init

---
+++ machine.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+CYAN="\e[36m"
+GREEN="\e[32m"
+MAGENTA="\e[35m"
+RED="\e[31m"
+YELLOW="\e[33m"
+ENDCOLOR="\e[0m"
+
+# Vérification que le script bash est lancer en root
+if [[ $(id -u) -ne 0 ]] ; then echo "Veuillez exécuter en mode root" ; exit 1 ; fi
+
+echo -e "${CYAN}[Lancement du script machine.sh]${ENDCOLOR}"
+sleep 3
+echo
+#-------------------------------------------------------------------------------------------------------
+echo -e "- ${YELLOW}[1/6]${ENDCOLOR} MAJ des dépots de Debian [${RED}✗${ENDCOLOR}]"
+sleep 2
+apt update
+echo -e "- ${YELLOW}[1/6]${ENDCOLOR} MAJ des dépots de Debian [${GREEN}✓${ENDCOLOR}]"
+echo
+#-------------------------------------------------------------------------------------------------------
+echo -e "- ${YELLOW}[2/6]${ENDCOLOR} MAJ des paquets [${RED}✗${ENDCOLOR}]"
+sleep 2
+apt upgrade -y
+echo -e "- ${YELLOW}[2/6]${ENDCOLOR} MAJ des paquets [${GREEN}✓${ENDCOLOR}]"
+echo
+#-------------------------------------------------------------------------------------------------------
+echo -e "- ${YELLOW}[3/6]${ENDCOLOR} Installation de paquets complémentaires [${RED}✗${ENDCOLOR}]"
+sleep 2
+apt install curl figlet git htop neofetch sudo wget -y
+echo -e "- ${YELLOW}[3/6]${ENDCOLOR} Installation de paquets complémentaires [${GREEN}✓${ENDCOLOR}]"
+echo
+#-------------------------------------------------------------------------------------------------------
+echo -e "- ${YELLOW}[4/6]${ENDCOLOR} Ajout d'un utilisateur jeremy [${RED}✗${ENDCOLOR}]"
+sleep 2
+adduser jeremy
+echo -e "- ${YELLOW}[4/6]${ENDCOLOR} Ajout d'un utilisateur jeremy [${GREEN}✓${ENDCOLOR}]"
+echo
+#-------------------------------------------------------------------------------------------------------
+echo -e "- ${YELLOW}[5/6]${ENDCOLOR} Ajout de l'utilisateur « jeremy » au groupe « sudo » [${RED}✗${ENDCOLOR}]"
+sleep 2
+usermod -aG sudo jeremy
+echo -e "- ${YELLOW}[5/6]${ENDCOLOR} Ajout de l'utilisateur « jeremy » au groupe « sudo » [${GREEN}✓${ENDCOLOR}]"
+echo
+#-------------------------------------------------------------------------------------------------------
+echo -e "- ${YELLOW}[6/6]${ENDCOLOR} Définition du locale fr_FR.UTF-8 [${RED}✗${ENDCOLOR}]"
+sleep 2
+localectl set-locale LANG=fr_FR.UTF-8
+echo -e "- ${YELLOW}[6/6]${ENDCOLOR} Définition du locale fr_FR.UTF-8 [${GREEN}✓${ENDCOLOR}]"
+echo
+#-------------------------------------------------------------------------------------------------------
+echo
+echo -e "${CYAN}[Fin du script machine.sh]${ENDCOLOR}"
---
+++ motd.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+CYAN="\e[36m"
+GREEN="\e[32m"
+MAGENTA="\e[35m"
+RED="\e[31m"
+YELLOW="\e[33m"
+ENDCOLOR="\e[0m"
+
+# Vérification que le script bash est lancer en root
+if [[ $(id -u) -ne 0 ]] ; then echo "Veuillez exécuter en mode root" ; exit 1 ; fi
+
+echo -e "${CYAN}[Lancement du script motd.sh]${ENDCOLOR}"
+sleep 3
+echo
+#-------------------------------------------------------------------------------------------------------
+echo -e "- ${YELLOW}[1/2]${ENDCOLOR} Installation du MOTD [${RED}✗${ENDCOLOR}]"
+sleep 2
+mkdir -p /root/.config/neofetch && touch /root/.config/neofetch/config.conf
+mkdir -p /etc/neofetch && touch /etc/neofetch/config.conf
+curl -s https://raw.githubusercontent.com/jturazzi/dotfiles-debian/main/neofetch.conf > /root/.config/neofetch/config.conf
+curl -s https://raw.githubusercontent.com/jturazzi/dotfiles-debian/main/neofetch.conf > /etc/neofetch/config.conf
+echo -e "- ${YELLOW}[1/2]${ENDCOLOR} Installation du MOTD [${GREEN}✓${ENDCOLOR}]"
+echo
+#-------------------------------------------------------------------------------------------------------
+echo -e "- ${YELLOW}[2/2]${ENDCOLOR} Configuration du MOTD [${RED}✗${ENDCOLOR}]"
+sleep 2
+echo "" > /etc/motd
+rm -rf /etc/motd /etc/update-motd.d/*
+tee -a /etc/update-motd.d/00-motd > /dev/null <<EOT 
+#!/bin/sh
+figlet $(hostname -s)
+neofetch --config /etc/neofetch/config.conf
+EOT
+echo -e "- ${YELLOW}[2/2]${ENDCOLOR} Configuration du MOTD [${GREEN}✓${ENDCOLOR}]"
+echo
