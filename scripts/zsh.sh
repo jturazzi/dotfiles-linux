4bcfa98f161aaa8c2d1c1de31e5fc8b0490a4aa7
Jérémy Turazzi <git@jtrz.fr>
2023-03-20 18:51:52 +0100

    Rename files

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -40,9 +40,9 @@ echo
 #-------------------------------------------------------------------------------------------------------
 echo -e "- ${YELLOW}[5/5]${ENDCOLOR} Copie des configurations ZSH [${RED}✗${ENDCOLOR}]"
 sleep 2
-curl -s https://jturazzi.github.io/dotfiles-linux/.aliases > ~/.aliases
-curl -s https://jturazzi.github.io/dotfiles-linux/.p10k.zsh > ~/.p10k.zsh
-curl -s https://jturazzi.github.io/dotfiles-linux/.zshrc > ~/.zshrc
+curl -s https://jturazzi.github.io/dotfiles-linux/aliases > ~/.aliases
+curl -s https://jturazzi.github.io/dotfiles-linux/p10k.zsh > ~/.p10k.zsh
+curl -s https://jturazzi.github.io/dotfiles-linux/zshrc > ~/.zshrc
 echo -e "- ${YELLOW}[5/5]${ENDCOLOR} Copie des configurations ZSH [${GREEN}✓${ENDCOLOR}]"
 echo
 #-------------------------------------------------------------------------------------------------------
